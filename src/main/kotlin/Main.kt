ad0c870d1cae4ae9f6755225993fdd824cb2b8fc
AhnYeonghoo <dksdudgn00@naver.com>
2022-10-28 10:20:32 +0900

    Koltin Basic Grammar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -28,3 +28,11 @@ object TheThing {
         return ret
     }
 }
+
+object MyStringComparator : Comparator<String> {
+    override fun compare(o1: String, o2: String): Int {
+        if (o1.isEmpty() || o2.isEmpty())
+            return o1.length - o2.length
+        return o1[0] - o2[0]
+    }
+}
